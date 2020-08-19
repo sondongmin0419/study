535011223a7e6791fd8471d30f1d2981c238f4b0
asdsd9852 <asdsd9852@naver.com>
2020-08-19 22:44:23 +0900

    0819 study

--- python/b_17609_2.py
+++ python/b_17609_2.py
@@ -2,25 +2,20 @@ T = int(input())
 
 for TC in range(T):
     st = list(input())
-    st_rev = st[::-1]
-    result = 1
+    st_rev = st[::-1]          #문자열 reverse
     if st == st_rev:   #회문판별
         print(0)
-        continue
-    else:
+    else:   # 회문아닐경우
         i = 0
-        while i <= len(st) // 2:
-            if st[i] != st_rev[i]:
-                if st[i+1:len(st)//2+1] == st_rev[i:len(st)//2]:
-                    result = 0
+        while i <= len(st) // 2:   # i가 0부터 문자열길이의 절반까지만 조회
+            if st[i] != st_rev[i]:  # 문자열과 reverse한 문자열을 차레로 비교하며 다를경우
+                if st[i+1:len(st)//2+1] == st_rev[i:len(st)//2]: # 기존문자열 i+1인덱스부터 (절반+1)까지 인덱스와
+                    print(1)                                   #reverse한 문자열 그대로 비교해서
                     break
-                elif st[i:len(st)//2] == st_rev[i+1:len(st)//2+1]:
-                    result = 0
+                elif st[i:len(st)//2] == st_rev[i+1:len(st)//2+1]: #기존 문자열 그대로와 reverse한 문자열 i+1부터비교
+                    print(1)
                     break
                 else:
+                    print(2)
                     break
             i += 1
-        if result == 0:
-            print(1)
-        else:
-            print(2)
