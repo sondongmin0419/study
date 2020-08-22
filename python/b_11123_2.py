cac8d0a975cee131883d7e921a3c3dd92f77649e
sondongmin0419 <dongmin.dev0419@gmail.com>
2020-08-22 23:24:21 +0900

    0822 study

--- python/b_11123_2.py
+++ python/b_11123_2.py
@@ -1,38 +1,33 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-
 def group(h, w):
+    total = 0
     if li[h][w] == ".":
         return 0
-    elif li[h][w] == "#":
+    else:
         li[h][w] = "@"
         if w + 1 != W:
-            if li[h][w+1] == "#":
-                group(h, w+1)
+            if li[h][w + 1] == "@":
+                total += 1
+            if li[h][w + 1] == "#":
+                li[h][w] = "@"
         if h + 1 != H:
+            if li[h + 1][w] == "@":
+                total += 1
             if li[h+1][w] == "#":
-                group(h+1, w)
+                li[h+1][w] = "@"
         if w != 0:
-            if li[h][w-1] == "#":
-                group(h, w-1)
+            if li[h][w - 1] == "@":
+                total += 1
+            if li[h][w - 1] == "#":
+                li[h][w - 1] = "@"
         if h != 0:
+            if li[h-1][w] == "@":
+                total += 1
             if li[h-1][w] == "#":
-                group(h-1, w)
+                li[h-1][w] = "@"
+
+    if total == 0:
         return 1
     else:
-        if w + 1 != W:
-            if li[h][w+1] == "#":
-                group(h, w+1)
-        if h + 1 != H:
-            if li[h+1][w] == "#":
-                group(h+1, w)
-        if w != 0:
-            if li[h][w-1] == "#":
-                group(h, w-1)
-        if h != 0:
-            if li[h-1][w] == "#":
-                group(h-1, w)
         return 0
 
 
@@ -55,4 +50,5 @@ for TC in range(T):
     for high in range(H):
         for wide in range(W):
             cnt += group(high, wide)
+
     print(cnt)
