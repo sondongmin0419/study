8c3b7fd785b26cfbce77feb15b7a4ecdddc40e02
sondongmin0419 <dongmin.dev0419@gmail.com>
2020-12-10 22:36:23 +0900

    1210 study

--- python/b_5052 전화번호 목록.py
+++ python/b_5052 전화번호 목록.py
@@ -4,17 +4,13 @@ input = sys.stdin.readline
 
 def YESorNo():
     n = int(input())
-    numbers = {}
-    last_w = 0
-    for i in range(n):
-        num = input().rstrip()
-        current_dict = numbers
-        for w in num:
-            if last_w in current_dict:
+    phone_num = {input().rstrip() for i in range(n)}
+    for num in phone_num:
+        for j in range(1, len(num)):
+            if num[0:j] in phone_num:
                 return False
-            current_dict = current_dict.setdefault(w, {})
-        current_dict[last_w] = last_w
     return True
+
 
 t = int(input())
 
