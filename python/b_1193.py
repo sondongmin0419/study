3c2350412689877813c093732321ba218e0fcc20
sondongmin0419 <dongmin.dev0419@gmail.com>
2020-12-22 21:31:22 +0900

    1222 study

--- python/b_1193.py
+++ python/b_1193.py
@@ -1,29 +1,15 @@
+import math
+
 n = int(input())
 
 i = 0
 total = 0
-while True:
-    i += 1
-    n = n - i
-    if n > i+1:
-        continue
-    else:
-        i += 1
-        break
-if i % 2 == 1:
-    r = i
-    c = 1
+
+k = int(math.sqrt(n * 2))
+while k * (k + 1)//2 < n:
+    k += 1
+p = k * (k + 1)//2 - n
+if k % 2 == 0:
+    print(str(k - p) + '/' + str(1 + p))
 else:
-    c = i
-    r = 1
-
-while n > 1:
-    n -= 1
-    if i % 2 == 1:
-        r -= 1
-        c += 1
-    else:
-        r += 1
-        c -= 1
-
-print(f'{r}/{c}')
+    print(str(1 + p) + "/" + str(k - p))
